271175b8987a7ca929ba576ffe718082fdb6847a
Dillon Schaffer <dschaffe@calpoly.edu>
2024-11-11 12:08:11 -0800

    Lab 7

--- command_line.py
+++ command_line.py
@@ -1,1 +1,12 @@
+import sys
 
+import convert
+
+sum = 0
+for idk in range(1, len(sys.argv)):
+    float_num = convert.str_to_float(sys.argv[idk])
+    if float_num != None:
+        sum += float_num
+
+if __name__ == '__main__':
+    print(sum)
--- convert.py
+++ convert.py
@@ -1,1 +1,7 @@
+def str_to_float(word:str) -> float or None:
+    try:
+        word = float(word)
+        return word
+    except ValueError:
+        return None
 
