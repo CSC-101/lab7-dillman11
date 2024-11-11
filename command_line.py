c2b270507603aedf74d6f236cf2f8d64ccf028a9
Dillon Schaffer <dschaffe@calpoly.edu>
2024-11-11 15:24:45 -0800

    Lab 7

--- command_line.py
+++ command_line.py
@@ -7,6 +7,7 @@ for idk in range(1, len(sys.argv)):
     float_num = convert.str_to_float(sys.argv[idk])
     if float_num != None:
         sum += float_num
+print(sum)
 
 if __name__ == '__main__':
-    print(sum)
+    print(sys.argv)
