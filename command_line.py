8080313189ed94a6424facb677708f29293c782d
Dillon Schaffer <dschaffe@calpoly.edu>
2024-11-11 20:37:23 -0800

    Lab 7

--- command_line.py
+++ command_line.py
@@ -4,9 +4,9 @@ import convert
 
 sum = 0
 for idk in range(1, len(sys.argv)):
-    float_num = convert.str_to_float(sys.argv[idk])
-    if float_num != None:
-        sum += float_num
+    num = convert.str_to_float(sys.argv[idk])
+    if num != None:
+        sum += num
 
 if __name__ == '__main__':
     print(sum)
